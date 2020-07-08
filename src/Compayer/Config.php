ccf600092a2da0a5aad801255a414b4409d21fde
EvgeniyStrigo <evgeniy.strigo@protocol.one>
2020-07-08 15:39:33 +0300

    Data API host url changed again

--- src/Compayer/Config.php
+++ src/Compayer/Config.php
@@ -36,7 +36,7 @@ class Config
     {
         $this->clientId = $clientId;
         $this->secretKey = $secretKey;
-        $this->eventApiUrl = 'https://data.compayer.com';
+        $this->eventApiUrl = 'https://receiver.compayer.com';
         $this->transport = new Guzzle();
         $this->sandboxMode = false;
         $this->debugMode = false;
